c9d35cfa590565610e6f19cd4fe3205aa2893002
Jay <mailer_wombats.0u@icloud.com>
2024-10-09 13:46:02 -0700

    1.1 update

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-# Limit Texture Size Menu Addon for Blender
-A simple Blender addon that adds a menu in the 3D view header to limit the viewport texture size.
+# Limit Texture Size in Eevee
+Adds a subpanel to the Eevee Simplify panel allowing you to limit texture sizes.
+
+## Changelog
+1.1 - Moved texture limit menu from 3D view to the Eevee Simplify panel
+1.0 - Initial release
+
+## Known Limititations
+When turning Simplify off, the texture limit is set to off, but stays off when reenabling Simplify. I'm working to fix this. You can still manually set your texture limit when enabling Simplify.
 
 Also available on [Gumroad](http://stoatsnhoney.gumroad.com).
--- __init__.py
+++ __init__.py
@@ -27,57 +27,64 @@ class OBJECT_OT_limit_texture_size(bpy.types.Operator):
 
     sizes = [
         ('CLAMP_OFF', 'Off', 'No limit'),
-        ('CLAMP_128', '128 px', '128 px'),
-        ('CLAMP_256', '256 px', '256 px'),
-        ('CLAMP_512', '512 px', '512 px'),
-        ('CLAMP_1024', '1024 px', '1024 px'),
-        ('CLAMP_2048', '2048 px', '2048 px'),
-        ('CLAMP_4096', '4096 px', '4096 px'),
-        ('CLAMP_8192', '8192 px', '8192 px')
+        ('CLAMP_128', '128', '128 px'),
+        ('CLAMP_256', '256', '256 px'),
+        ('CLAMP_512', '512', '512 px'),
+        ('CLAMP_1024', '1024', '1024 px'),
+        ('CLAMP_2048', '2048', '2048 px'),
+        ('CLAMP_4096', '4096', '4096 px'),
+        ('CLAMP_8192', '8192', '8192 px')
     ]
 
     size: bpy.props.EnumProperty(
-        name="Texture Size Limit",
+        name="Texture Limit",
         description="Limit the texture size in the viewport",
         items=sizes,
-        default='CLAMP_OFF'
+        default='CLAMP_OFF',
+        update=lambda self, context: self.update_texture_limit(context)
     )
 
-    def execute(self, context):
+    def update_texture_limit(self, context):
         context.preferences.system.gl_texture_limit = self.size
+        context.area.tag_redraw()
         self.report({'INFO'}, f"Texture size limited to {self.size.replace('CLAMP_', '')} px" if self.size != 'CLAMP_OFF' else "Texture size limit turned off")
-        return {'FINISHED'}
 
     def invoke(self, context, event):
         return context.window_manager.invoke_props_dialog(self)
 
 
-class VIEW3D_MT_limit_menu(bpy.types.Menu):
-    bl_label = "Textures"
-    bl_idname = "VIEW3D_MT_limit_menu"
+class RENDER_PT_limit_texture_size(bpy.types.Panel):
+    """Creates a subpanel in the Simplify panel in Render Properties"""
+    bl_label = "Texture Limit"
+    bl_idname = "RENDER_PT_limit_texture_size"
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+    bl_context = "render"  # This is the Render Properties tab
+    bl_parent_id = "RENDER_PT_simplify"  # Parent to Simplify panel
 
     def draw(self, context):
         layout = self.layout
-        layout.operator(OBJECT_OT_limit_texture_size.bl_idname)
+        render_settings = context.scene.render
 
-
-def draw_item(self, context):
-    layout = self.layout
-    layout.menu(VIEW3D_MT_limit_menu.bl_idname)
+        # Check if Simplify is enabled
+        if not render_settings.use_simplify:
+            # Set texture limit to 'Off' if Simplify is disabled
+            context.preferences.system.gl_texture_limit = 'CLAMP_OFF'
+        
+        # Add the texture limit dropdown
+        row = layout.row()
+        row.prop(context.preferences.system, "gl_texture_limit", text="Texture Limit")
+        row.enabled = render_settings.use_simplify  # Enable only if Simplify is on
 
 
 def register():
     bpy.utils.register_class(OBJECT_OT_limit_texture_size)
-    bpy.utils.register_class(VIEW3D_MT_limit_menu)
-
-    bpy.types.VIEW3D_MT_editor_menus.append(draw_item)
+    bpy.utils.register_class(RENDER_PT_limit_texture_size)
 
 
 def unregister():
     bpy.utils.unregister_class(OBJECT_OT_limit_texture_size)
-    bpy.utils.unregister_class(VIEW3D_MT_limit_menu)
-
-    bpy.types.VIEW3D_MT_editor_menus.remove(draw_item)
+    bpy.utils.unregister_class(RENDER_PT_limit_texture_size)
 
 
 if __name__ == "__main__":
